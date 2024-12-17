eb669ebc1081f8a0f2c5aa4cb9269da31539d1c2
naechutimon <chutimonnaenae@gmail.com>
2024-12-17 18:41:56 +0700

    chune10/3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,46 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+        int count = 0;
+        float sum = 0;
+        float sum_of_square = 0;
+
+        string textline;
+        ifstream source("score.txt");
+        
+        if (!source){
+            cout << "Error: Cannot open file 'score.txt'" << "\n";
+            return 1;
+        }
+
+        while (getline(source,textline))
+
+        {
+                float value = stof(textline);
+                sum += value;
+                sum_of_square += value*value;
+                count++;
+
+        }
+        
+        float mean = sum/count;
+        float dev  = sqrt((sum_of_square / count) - (mean * mean));
+
+        cout << "Number of data = " << count << "\n";
+
+        cout << setprecision(3);
+
+        cout << "Mean = " << mean << "\n";
+
+        cout << "Standard deviation = " << dev << "\n";
+
+}
